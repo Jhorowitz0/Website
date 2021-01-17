2e4761f8395456d88b5878be74bc2bb1bf8b10e5
Jhorowitz0 <46899492+Jhorowitz0@users.noreply.github.com>
2021-01-17 16:12:44 -0500

    added navigation to project pages

--- js/project.js
+++ js/project.js
@@ -11,3 +11,105 @@ var flkty = new Flickity( elem, {
 var flkty = new Flickity( '.main-carousel', {
   // options
 });
+
+
+var darkColor = "#050517";
+var lightColor = "#D0D6DD";
+var highlight = "#E01A4F";
+var navColor = darkColor;
+
+var navElements = document.querySelectorAll(".nav-item"); // item containing both title and marker
+var navTitles = document.querySelectorAll(".nav-title"); // the title of each section in the nav 
+var markerList = document.querySelectorAll(".nav-marker"); // the line that marks where on the page you are
+var navPositions = [];
+
+var curPage = 0;
+
+function highlightElem(elem){
+    elem.style.backgroundColor=highlight;
+    elem.style.animationFillMode="forwards";
+    elem.style.animation="grow 0.05s ease-in"
+    elem.style.width="40px";
+    elem.style.transform="translateX(120px) rotate(90deg)";
+}
+
+function unhighlightElem(elem){
+    elem.style.backgroundColor=navColor;
+    elem.style.animationFillMode="forwards";
+    elem.style.animation="shrink 0.05s ease-in"
+    elem.style.width="30px";
+    elem.style.transform="";
+}
+
+function updateNav(){
+  curPage = 0;
+  let pos = window.pageYOffset;
+
+  let pageSections = [];
+  for(ELEM of document.querySelectorAll("section")){
+    // let elemPos = ELEM.getBoundingClientRect().top;
+    // let elemHeigh = ELEM.offsetHeight;
+
+    // if(elemPos > 0){
+    //   curPage++;
+    // }
+    pageSections.push(ELEM);
+  }
+  for(i in pageSections){
+    let elemPos = pageSections[i].getBoundingClientRect().top - 50;
+
+    if(i == pageSections.length-1 && elemPos < 200){
+      curPage = i;
+      break;
+    }
+
+    if(i > 0 && elemPos>0){
+      curPage = i-1;
+      break;
+    }
+  }
+
+  
+  for(let i = 0; i < markerList.length; i++){
+    if(i == curPage){
+      highlightElem(markerList[i]);
+      navTitles[i].style.color=highlight;
+      navTitles[i].style.opacity=1;
+    }
+    else{
+        unhighlightElem(markerList[i]);
+        navTitles[i].style.color=navColor;
+        navTitles[i].style.opacity=0;
+        if(navElements[i].matches(':hover')) navTitles[i].style.opacity=1;
+    }
+  }
+
+}
+
+
+function onScroll(){
+    updateNav();
+}
+
+function onPointerMove(){
+  for(let i = 0; i < markerList.length; i++){
+    if(navElements[i].matches(':hover')) navTitles[i].style.opacity=1;
+    else if(i != curPage)navTitles[i].style.opacity=0;
+  //   if(i == curPage){
+  //     highlightElem(markerList[i]);
+  //     navTitles[i].style.color=highlight;
+  //     navTitles[i].style.opacity=1;
+  //   }
+  //   else{
+  //       unhighlightElem(markerList[i]);
+  //       navTitles[i].style.color=navColor;
+  //       navTitles[i].style.opacity=0;
+  //       if(navElements[i].matches(':hover')) navTitles[i].style.opacity=1;
+  //   }
+  }
+  
+}
+
+updateNav();
+document.addEventListener("scroll",onScroll,false);
+document.addEventListener("pointermove",onPointerMove,false);
