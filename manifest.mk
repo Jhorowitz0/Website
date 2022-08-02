3a8cd89a8d2ee7bb79cf6f86cf99eaff764ff197
jhorowitz99 <contact@josephbhorowitz.com>
2022-08-02 07:16:42 -0400

    Create manifest.mk

---
+++ manifest.mk
@@ -0,0 +1,12 @@
+#Main Page
+##Header
+* Profile Pic
+* Hi, My name is Joseph Horowitz
+* Job title
+* linkedin, hover email, resume
+* down arrow
+##Projects
+* project title
+* img
+* short description
+* 
